bb9e9d5608dbf6b261f8bd0a094b54e318544917
Ben Aherne <bta@outlook.ie>
2018-11-23 19:19:14 +0000

    what

---
+++ test/algorithmstest.js
@@ -0,0 +1,37 @@
+var assert = require('assert');
+
+var x = 1
+
+var y = function (x) {
+    return x + x;
+}
+
+function calculator(x, y) {
+    return y(x)
+}
+
+var passable = function (operand) {
+    return operand + operand
+}
+var trap = passable
+describe('Basic Mocha String Test', function () {
+    it('should return 2', function () {
+        assert.equal(y(x), 2);
+    });
+    it('should return 2', function () {
+        assert.equal(calculator(x, y), 2);
+    });
+    it('should return 2',5 function () {
+        assert.equal(calculator(x, function (operand) {
+            return operand + operand
+        }), 2);
+    });
+    it('should return 2', function () {
+        assert.equal(calculator(x, passable), 2);
+    });
+    it('should return 2', function () {
+        assert.equal(calculator(x, trap), 2);
+    });
+
+});
+
