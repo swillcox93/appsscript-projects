8526db5278a81902d2d41b004206fd0d19c47ed2
Scott <swillcox93@gmail.com>
2018-12-05 22:40:53 +0000

    Update Table.vue

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -5,7 +5,9 @@
       <input id="uinput" step="1E-1" type="number" v-model="uinput"/>
       <label for="rows">rows</label>
       <input id="rows" type="number" v-model="rows"/>
-      <!-- put textfield here -->
+      <!--TextArea-->
+      <textarea id="noiseArr" v-model="noiseArr"></textarea>
+
     </form>
 
     <b-table class="td" striped hover :items="provider"></b-table>
@@ -13,41 +15,25 @@
 </template>
 
 <script>
-  import {GraphData} from "../lib/GraphData";
-  // import 'bootstrap/dist/css/bootstrap.css'
-  // import 'bootstrap-vue/dist/bootstrap-vue.css'
-  let gd = new GraphData(1.00E-03, 1, 1, 2, 0.1, 0.5, 0.5, 1);
+  import {GraphData} from "@/lib/GraphData";
 
-  // function provider(input) {
-  //   return new GraphData(input, 1, 1, 2, 0.1, 0.5, 0.5, 1).rows(8);
-  // }
-
-  console.log(gd);
-     const randoms = [
-              0.33599008,
-              0.7254017357,
-              0.538389962,
-              0.9808202199,
-              0.2957488929,
-              0.8504724979,
-              0.1857151908,
-              0.9443163872,
-          ];
+  console.log(GraphData);
   export default {
 
     data(){
       return {
         uinput: 1E-3,
-        rows: 8
+        rows: 8,
+        noiseArr: Math.random()+""
       }
-    },
+    },  
 
     computed: {
       provider() {
-        return new GraphData(this.uinput, 1, 1, 2, 0.1, 0.5, 0.5, 1)
-          .rows(this.rows, randoms);
+        return new GraphData(this.uinput, 1, 1, 2, 0.1, 0.5, 0.5, 1).rows(this.rows, this.noiseArr.split("\n"));
+
       }
-    }
+    }  
   }
 </script>
 
