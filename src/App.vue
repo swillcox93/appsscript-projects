eccdfcb0abaf95137c7799bea4e771c89535003a
Scott <swillcox93@gmail.com>
2019-03-26 22:22:01 +0000

    .j

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,54 @@
 <template>
   <div id="app">
+    <sidebar-menu :menu="menu" />
     <h1>Basic Pharmalogical Equations</h1>
     <router-link to="/table">Table</router-link>
-    <router-view/>
+    <router-view/>s
   </div>
 </template>
 
 <script>
-export default {
+/*export default {
   name: 'App'
-}
+}*/
+export default {
+        name: 'App',
+        data() {
+            return {
+                menu: [
+                    {
+                        header: true,
+                        title: 'Main Navigation',
+                        // component: componentName
+                        // visibleOnCollapse: true
+                    },
+                    { // item
+                        href: '/',
+                        title: 'Dashboard',
+                        icon: 'fa fa-user',
+                        // disabled: true
+                        /*
+                        badge: {
+                            text: 'new',
+                            // class:''
+                        }
+                        */
+                    },
+                    { // item with child
+                        href: '/charts',
+                        title: 'Charts',
+                        icon: 'fa fa-chart-area',
+                        child: [
+                            {
+                                href: '/charts/sublink',
+                                title: 'Sub Link',
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <style>
